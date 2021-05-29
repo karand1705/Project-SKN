92b6b80c4ece73a8a727aa1140b4281a1bf462fc
Karan Dewani <dewani1705@gmail.com>
2021-05-29 16:07:13 +0530

    first commit

---
+++ EC2/ec2.tf
@@ -0,0 +1,50 @@
+# creating a key pair
+variable "key_name" {}
+
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+# saving key to local file
+resource "local_file" "deploy-key" {
+    content  = tls_private_key.example.private_key_pem
+    filename = "C:/Users/karan/tf_key.pem"
+}
+
+
+
+
+variable "pri_subnet_id_A" {}
+
+resource "aws_instance" "web_A" {
+    ami = "ami-09a7bbd08886aafdf"
+    instance_type = "t2.micro"
+    subnet_id = var.pri_subnet_id_A
+    vpc_security_group_ids = [aws_security_group.InternalWeb.id]
+    key_name = aws_key_pair.generated_key.key_name
+    iam_instance_profile = aws_iam_instance_profile.test_profile.name
+}
+
+
+
+variable "pri_subnet_id_B" {}
+
+resource "aws_instance" "web_B" {
+    ami = "ami-09a7bbd08886aafdf"
+    instance_type = "t2.micro"
+    subnet_id = var.pri_subnet_id_B
+    vpc_security_group_ids = [aws_security_group.InternalWeb.id]
+    key_name = aws_key_pair.generated_key.key_name
+    iam_instance_profile = aws_iam_instance_profile.test_profile.name
+}
+
+
+
+
---
+++ main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+     region = "ap-south-1"
+     profile = "project-skn"
+}
+
+variable "Repo_Link" {
+  description = "Application Code URL"
+  type        = string
+} 
+
+variable "Repo_Name" {
+  description = "The repo name in which code is present"
+  type        = string
+} 
+
+module "My_VPC" {
+     source = "./VPC"
+     subnet_id_A = module.my_AZ_1a.sub_public_A
+     subnet_id_B = module.my_AZ_1b.sub_public_B
+     pub_AZ_A = module.my_AZ_1a.sub_public_A
+     pub_AZ_B = module.my_AZ_1b.sub_public_B
+     private_AZ_A = module.my_AZ_1a.sub_App_A
+     private_AZ_B = module.my_AZ_1b.sub_App_B
+     Data_AZ_A = module.my_AZ_1a.sub_Data_A
+     Data_AZ_B = module.my_AZ_1b.sub_Data_B
+}
+
+
+module "my_AZ_1a" {
+     source = "./AZ-1a"
+     my_Vpc_id = module.My_VPC.id
+
+
+     
+}
+
+
+module "my_AZ_1b" {
+     source = "./AZ-1b"
+     my_Vpc_id_B = module.My_VPC.id
+}
+
+
+
+module "ec2" {
+     source = "./EC2"
+     key_name = "my-key"
+     pub_subnet_id_A = module.my_AZ_1a.sub_public_A
+     pub_subnet_id_B = module.my_AZ_1b.sub_public_B
+     pri_subnet_id_A = module.my_AZ_1a.sub_App_A
+     pri_subnet_id_B = module.my_AZ_1b.sub_App_B
+     Vpc_id_sg = module.My_VPC.id
+     Git_url = var.Repo_Link
+     Git_folder = var.Repo_Name
+}
